04aa39efefe3d3a8a0eef4b8e707babcb4381866
Lucas030504 <lbarroscarregozi@gmail.com>
2024-02-28 15:55:53 -0300

    commit

--- js/imun-trib.js
+++ js/imun-trib.js
@@ -36,7 +36,7 @@ function enviarImunTrib() {
     }
 
     // formando mensagem
-    var mensagem = `Olá${leadImunTribMsgEstruturada}! Tudo bem? Me chamo ${agenteImunTrib.value} e faço parte da Projetos Consultoria Integrada, empresa júnior de Brasília.%0A%0AVocê sabia que é possível nunca mais pagar o imposto ISS? Além disso, ${ejImunTribMsgEstruturada} receber a restituição dos últimos 5 anos do valor pago com correção monetária. Buscamos isso realizando uma ação de imunidade tributária, totalmente de acordo com a lei.%0A%0APodemos marcar uma reunião${reuniaoImunTribMsgEstruturada} para explicarmos um pouco melhor? Vou lhe enviar uma matéria falando um pouco sobre como a projetos conseguiu ter êxito nesse processo.`
+    var mensagem = `Olá${leadImunTribMsgEstruturada}! Tudo bem? Me chamo ${agenteImunTrib.value} e faço parte da Projetos Consultoria Integrada, empresa júnior de Brasília.%0A%0AVocê sabia que é possível nunca mais pagar o imposto ISS? Além disso, ${ejImunTribMsgEstruturada} receber a restituição dos últimos 5 anos do valor pago com correção monetária. Buscamos isso realizando uma ação de imunidade tributária, totalmente de acordo com a lei.%0A%0APodemos marcar uma reunião${reuniaoImunTribMsgEstruturada} para explicarmos um pouco melhor? Vou lhe enviar uma matéria falando um pouco sobre como a Projetos conseguiu ter êxito nesse processo.`
 
     // url que vai ser aberta ao clicar no botão enviar
     var msg = `https://wa.me/55${zapImunTrib.value}/?text=${mensagem}`
